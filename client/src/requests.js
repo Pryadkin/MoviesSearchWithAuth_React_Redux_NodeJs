a9feb088d8d1d988e4bf6b2d7d7626e1a731d357
Anton Pryadkin <zorg200687@gmail.com>
2020-05-14 15:31:03 +0300

    correct button isWithPicture

--- client/src/requests.js
+++ client/src/requests.js
@@ -1,6 +1,6 @@
 import api from './utils/api';
 
-export const getMovie = async (name, page, isWithPicture) => {
+export const getMovie = async (name, isWithPicture) => {
   try {
     const response = await api.get("/", {
       params: {
