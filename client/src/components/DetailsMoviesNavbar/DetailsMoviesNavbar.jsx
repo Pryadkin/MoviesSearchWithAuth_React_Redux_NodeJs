b35a226b40d1d7c2c7f9854f4dee9b2c2faa031d
Anton Pryadkin <zorg200687@gmail.com>
2020-06-01 01:10:12 +0300

    completed the DetailsMovie component

--- client/src/components/DetailsMoviesNavbar/DetailsMoviesNavbar.jsx
+++ client/src/components/DetailsMoviesNavbar/DetailsMoviesNavbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
 import styles from "./DetailsMoviesNavbar.module.scss";
@@ -7,30 +7,33 @@ import styles from "./DetailsMoviesNavbar.module.scss";
 const DetailsMoviesNavbar = () => {
 
   return (
+
     <Navbar
       bg="dark"
       variant="dark"
       className={styles.navbar}
     >
-      <Navbar.Brand className={styles.navbar_brand}>
-        Details Movies
-      </Navbar.Brand>
+      <Container>
+        <Navbar.Brand className={styles.navbar_brand}>
+          Details Movies
+        </Navbar.Brand>
 
-      <Nav className="mr-auto">
-        <LinkContainer to="/profile">
-          <Nav.Link>
-            Home
-         </Nav.Link>
-        </LinkContainer>
+        <Nav className="mr-auto">
+          <LinkContainer to="/profile">
+            <Nav.Link>
+              Home
+            </Nav.Link>
+          </LinkContainer>
 
-
-        <LinkContainer to="/search">
-          <Nav.Link>
-            Search
-          </Nav.Link>
-        </LinkContainer>
-      </Nav>
+          <LinkContainer to="/search">
+            <Nav.Link>
+              Search
+            </Nav.Link>
+          </LinkContainer>
+        </Nav>
+      </Container>
     </Navbar>
+
   )
 }
 
--- client/src/pages/DetailsMovie/DetailsMovie.jsx
+++ client/src/pages/DetailsMovie/DetailsMovie.jsx
@@ -2,9 +2,14 @@ import React, { useEffect, useState } from 'react';
 import DetailsMoviesNavbar from '../../components/DetailsMoviesNavbar/DetailsMoviesNavbar';
 import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { getDetailsMovie, cleanDetails } from '../../redux/actions';
+import { getDetailsMovie } from '../../redux/actions';
+import ReactStars from 'react-stars';
+import getStyledBudget from './helpers/getStyledBudget';
 
-import { Fade, Button } from 'react-bootstrap';
+import { Container, Row, Col, Image } from 'react-bootstrap';
+import dollarIcon from '../../img/icons/dollarIcon.png';
+import styles from './DetailsMovie.module.scss';
+import cx from 'classnames';
 
 const DetailsMovie = () => {
   const { id } = useParams();
@@ -17,56 +22,129 @@ const DetailsMovie = () => {
       title,
       overview,
       poster_path,
-      release_date
+      release_date,
+      runtime,
+      vote_average,
+      revenue,
+      budget
     } = detailsMovie;
 
     return (
-      <>
+      <Container className={cx(styles.container, "p-0 px-5 px-md-0")}>
+        <Row>
+          <Col lg={4} md={6} className="d-flex justify-content-center justify-content-sm-center">
+            <Image className={styles.poster} src={poster_path} alt={title} />
+          </Col>
 
+          <Col lg={8} md={6}>
+            <h2>
+              {title}
+            </h2>
 
-        <div id="example-fade-text">
-          <h2>
-            {title}
-          </h2>
+            <ul>
+              {
+                release_date
+                  ?
+                  <li>
+                    <div className={styles.list_title}>
+                      Release Date:
+                    </div>
+                    <div>
+                      {release_date}
+                    </div>
+                  </li>
+                  : null
+              }
 
-          <img src={poster_path} alt="" />
+              {
+                runtime
+                  ?
+                  <li>
+                    <div className={styles.list_title}>
+                      Runtime:
+                    </div>
+                    <div>
+                      {runtime} minutes
+                    </div>
+                  </li>
+                  : null
+              }
 
-          <p>{overview}</p>
-        </div>
+              {
+                budget
+                  ?
+                  <li>
+                    <div className={styles.list_title}>
+                      Budget:
+                    </div>
+                    <div>
+                      <img
+                        className={styles.dollar_icon}
+                        src={dollarIcon}
+                        alt="dollarIcon"
+                      />
+                      {getStyledBudget(budget)}
+                    </div>
+                  </li>
+                  : null
+              }
 
+              {
+                revenue
+                  ?
+                  <li>
+                    <div className={styles.list_title}>
+                      Revenue:
+                    </div>
+                    <div>
+                      <img
+                        className={styles.dollar_icon}
+                        src={dollarIcon}
+                        alt="dollarIcon"
+                      />
+                      {getStyledBudget(revenue)}
+                    </div>
+                  </li>
+                  : null
+              }
+            </ul>
 
-      </>
+            {
+              vote_average
+                ?
+                <ReactStars
+                  className="mt-0 mb-5"
+                  count={10}
+                  edit={false}
+                  value={vote_average}
+                  size={30}
+                  color2={'gold'}
+                  half
+                />
+                : null
+            }
+
+            {
+              overview
+                ?
+                <p>{overview}</p>
+                : null
+            }
+          </Col>
+        </Row>
+      </Container>
     )
   };
 
   useEffect(() => {
-
-    // dispatch(cleanDetails());
     dispatch(getDetailsMovie(id));
     setLoading(true);
-    // If this movie is located in the redux store, we don't dispatch an action.
-    // if (detailsMovie) {
-    //   if (detailsMovie.id !== +id) {
-    //     dispatch(getDetailsMovie(id));
-    //     setTimeout(() => {
-    //       setOpen(true);
-    //     }, 50)
-    //   }
-    // } else {
-
-    // }
-
-    // if (id === null) setOpen(false)
-
   }, [id]);
 
   return (
     <>
-
       <DetailsMoviesNavbar id={id} />
       {isLoading && detailsMovie ? <Details /> : "Loading"}
-
-
     </>
   )
 }
